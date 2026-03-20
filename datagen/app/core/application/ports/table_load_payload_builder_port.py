ecf29fd656b3b5f374622c4fd58cb9e3c1a25b7a
rmusin <rashat_musin@mail.ru>
2026-03-20 23:52:08 +0600

    change dict to EnginePair

---
+++ datagen/app/core/application/ports/table_load_payload_builder_port.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+from app.core.application.dto.publication import EngineLoadPayload
+from app.core.domain.entities import TableSpec
+
+
+class ITableLoadPayloadBuilder(ABC):
+    @abstractmethod
+    def build_load_payload(self, table: TableSpec) -> EngineLoadPayload:
+        pass
